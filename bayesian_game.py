406becec886038a1f5e32ae529cf577f8ebabde2
Noel <100097022+noel-campb@users.noreply.github.com>
2025-02-17 00:32:28 +0530

    Update bayesian_game.py

--- bayesian_game.py
+++ bayesian_game.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 name = input("Enter player name:")
 print(f"Your chosen name is: {name}")
@@ -8,7 +9,9 @@ if rename == "y":
 else:
     name = input("Enter player name:")
 
-print("#"*30,"\n",f"Hi {name}! Welcome to the bayesian game\n", "#"*30,"\n")
+print("#"*30)
+print(f"Hi {name}! Welcome to the bayesian game")
+print("#"*30,"\n")
 print("""
 There are 3 doors in front of you, labelled A, B and C.
 Behind one of these doors is a prize of $10000.
@@ -25,7 +28,9 @@ random_dict = dict(zip(keys, values))
 
 prize_door = [key for key, value in random_dict.items() if value == "$10000"][0]
 
-if first_choice == prize_door:
+if first_choice not in keys:
+    sys.exit("Invalid door selection")
+elif first_choice == prize_door:
     first_revelation = random.choice([i for i in keys if i != prize_door])
     print(f"\nYou have selected door {first_choice}")
     print(f"The game host opens door {first_revelation} to reveal dung")
